62bf0f60e4bf64778b6f4557dd100c893c4abc0b
Nick Pope <pope410211@gmail.com>
2015-06-09 22:22:35 -0400

    more jQuery

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,13 @@
 (function(window) {
 
-.getJSON("../../api/github/users/octocat/octocat.json")
+$.getJSON("../../api/github/users/octocat/octocat.json")
   .then(function(octocat) {
     console.log(octocat)
 
-    $avatar = $('')
+    $avatar = $('img .profilePic')
+    $avatar.attr('src', octocat.avatar_url);
+
+    $displayName = $()
   })
 
 
